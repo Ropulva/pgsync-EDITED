d2716021c8c2b651a85b473422a039782dbe9084
0abdullah0 <45294600+0abdullah0@users.noreply.github.com>
2025-04-27 03:22:42 +0300

    expr3. test sync

--- examples/pos/schema.py
+++ examples/pos/schema.py
@@ -21,51 +21,17 @@ class Item(Base):
     created_at: Mapped[datetime] = mapped_column(
         sa.DateTime, default=datetime.now()
     )
-    created_by: Mapped[str] = mapped_column(sa.String, unique=False, nullable=True)
     deleted: Mapped[bool] = mapped_column(sa.Boolean, default=False)
-    drug_index_id: Mapped[str] = mapped_column(sa.String, unique=False, nullable=True)
-    image_url: Mapped[str] = mapped_column(sa.String, unique=False, nullable=True)
-    name_ar: Mapped[str] = mapped_column(sa.String, unique=False, nullable=True)
     name_en: Mapped[str] = mapped_column(sa.String, unique=False, nullable=True)
     points: Mapped[int] = mapped_column(sa.Integer, default=0)
-    price: Mapped[float] = mapped_column(sa.Float, unique=False, nullable=True)
-    taxable: Mapped[bool] = mapped_column(sa.Boolean, default=False)
-    updated_at: Mapped[datetime] = mapped_column(
-        sa.DateTime, default=datetime.now()
-    )
-    updated_by: Mapped[str] = mapped_column(sa.String, unique=False, nullable=True)
-    workspace_id: Mapped[int] = mapped_column(sa.Integer)
-    international_barcode: Mapped[str] = mapped_column(sa.String, unique=False, nullable=True)
-    manufacture: Mapped[str] = mapped_column(sa.String, unique=False, nullable=True)
-    owner: Mapped[str] = mapped_column(sa.String, unique=False, nullable=True)
-    form: Mapped[str] = mapped_column(sa.String, unique=False, nullable=True)
-    number_of_strips: Mapped[int] = mapped_column(sa.Integer)
-    in_store_points: Mapped[int] = mapped_column(sa.Integer)
-    online_store_points: Mapped[int] = mapped_column(sa.Integer)
-    insurance_points: Mapped[int] = mapped_column(sa.Integer)
-    can_divide: Mapped[bool] = mapped_column(sa.Boolean, default=False)
-    gtn: Mapped[str] = mapped_column(sa.String, unique=False, nullable=True)
-    tax_value_percent: Mapped[int] = mapped_column(sa.Integer, default=0)
-    old_barcode: Mapped[str] = mapped_column(sa.String, unique=False, nullable=True)
 
 
 class ActiveIngredient(Base):
     __tablename__ = "active_ingredient"
     __table_args__ = (UniqueConstraint("id"),)
     id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
-    concentration: Mapped[str] = mapped_column(sa.String, unique=False, nullable=True)
-    created_at: Mapped[datetime] = mapped_column(
-        sa.DateTime, default=datetime.now()
-    )
-    created_by: Mapped[str] = mapped_column(sa.String, unique=False, nullable=True)
     deleted: Mapped[bool] = mapped_column(sa.Boolean, default=False)
-    drug_index_id: Mapped[str] = mapped_column(sa.String, unique=False, nullable=True)
     name: Mapped[str] = mapped_column(sa.String, unique=False, nullable=True)
-    updated_at: Mapped[datetime] = mapped_column(
-        sa.DateTime, default=datetime.now()
-    )
-    updated_by: Mapped[str] = mapped_column(sa.String, unique=False, nullable=True)
-    workspace_id: Mapped[int] = mapped_column(sa.Integer)
 
 class ItemActiveIngredients(Base):
     __tablename__ = "item_active_ingredients"
@@ -77,67 +43,12 @@ class ItemActiveIngredients(Base):
         Item,
         backref=sa.orm.backref("items"),
     )
-    active_ingredient_id: Mapped[int] = mapped_column(sa.Integer)
+    active_ingredient_id: Mapped[int] = mapped_column(sa.Integer, sa.ForeignKey(ActiveIngredient.id))
+    active_ingredients: Mapped[ActiveIngredient] = sa.orm.relationship(
+        ActiveIngredient,
+        backref=sa.orm.backref("active_ingredients"),
+    )
     
-
-class Tag(Base):
-    __tablename__ = "tag"
-    __table_args__ = (UniqueConstraint("id"),)
-    id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
-    deleted: Mapped[bool] = mapped_column(sa.Boolean, default=False)
-    name: Mapped[str] = mapped_column(sa.String, unique=False, nullable=True)
-    workspace_id: Mapped[int] = mapped_column(sa.Integer)
-
-class ItemTags(Base):
-    __tablename__ = "item_tags"
-    __table_args__ = (UniqueConstraint("id"),)
-    id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
-    workspace_id: Mapped[int] = mapped_column(sa.Integer)
-    item_id: Mapped[int] = mapped_column(sa.Integer, sa.ForeignKey(Item.id))
-    item: Mapped[Item] = sa.orm.relationship(
-        Item,
-        backref=sa.orm.backref("items"),
-    )
-    tag_id: Mapped[int] = mapped_column(sa.Integer, sa.ForeignKey(Tag.id))
-    tags: Mapped[Tag] = sa.orm.relationship(
-        Tag,
-        backref=sa.orm.backref("tags"),
-    )
-
-class Utilization(Base):
-    __tablename__ = "utilization"
-    __table_args__ = (UniqueConstraint("id"),)
-    id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
-    created_at: Mapped[datetime] = mapped_column(
-        sa.DateTime, default=datetime.now()
-    )
-    created_by: Mapped[str] = mapped_column(sa.String, unique=False, nullable=True)
-    deleted: Mapped[bool] = mapped_column(sa.Boolean, default=False)
-    drug_index_id: Mapped[str] = mapped_column(sa.String, unique=False, nullable=True)
-    name: Mapped[str] = mapped_column(sa.String, unique=False, nullable=True)
-    updated_at: Mapped[datetime] = mapped_column(
-        sa.DateTime, default=datetime.now()
-    )
-    updated_by: Mapped[str] = mapped_column(sa.String, unique=False, nullable=True)
-    workspace_id: Mapped[int] = mapped_column(sa.Integer)
-
-class ItemUses(Base):
-    __tablename__ = "item_uses"
-    __table_args__ = (UniqueConstraint("id"),)
-    id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
-    workspace_id: Mapped[int] = mapped_column(sa.Integer)
-    item_id: Mapped[int] = mapped_column(sa.Integer, sa.ForeignKey(Item.id))
-    item: Mapped[Item] = sa.orm.relationship(
-        Item,
-        backref=sa.orm.backref("items"),
-    )
-    use_id: Mapped[int] = mapped_column(sa.Integer, sa.ForeignKey(Utilization.id))
-    utilizations: Mapped[Utilization] = sa.orm.relationship(
-        Utilization,
-        backref=sa.orm.backref("utilizations"),
-    )
-
-
 
 @click.command()
 @click.option(
